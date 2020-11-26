8bc7ab841be8e28b8920f196957da00fc7ebcb8b
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-26 23:19:25 +0900

    11.26

--- game(do-while).c
+++ game(do-while).c
@@ -9,7 +9,7 @@ char b = '@';  // 검정색 알
 char w = 'O';  // 하얀색 알	
 char x = ' '; // 빈 칸
 char start; // 시작 버튼 	   
-
+int wR, wC, bR, bC; // 좌표입력을 위한 변수 
 
 // 출력 함수 
 	 
@@ -56,7 +56,7 @@ char init_othello(char a[N][N])
 		a[i][j]= x;
 	 }
 	 
-	 a[N/3]
+
 	 
 	
 }
@@ -70,29 +70,38 @@ void main() {
     printf("********othello**********\n"); // 게임시작을 알리는 문장 출력  
     printf("-> black stone : @\n"); //검정색 돌과 하얀색 돌이 무엇인지 알려주는 문장 출력  
     printf("-> white stone : O\n");	 
-	
+	printf(" if you want to start, input 'start' and press enter!  -->   "); // start를 입력하라는 문장 출력 
+  scanf("%c", &start); // start를 입력  
+  
+  
+     if( start ==start){ //start를 입력하면 게임이 시작됨  
+	 
 	 init_othello(gameboard);   // 초기 오델로 함수 호출 
 	 print_othello(gameboard); //출력 함수 - 초기 오델로 함수를 출력하도록 함  
-
-  printf(" if you want to start, input 'start' and press enter!  -->   ");
-  scanf("%c", &start);
+     }
   
-  
-     if( start ==start)
-  	 {
-	    printf("&& O turn &&\n");
-  	   printf("the number of O: \n");
-  	   printf("input coordinates:  ");
-  	   scanf("%i %i", &i, &j);
+  	 // 흰색 알의 차례일 경우  // 
+	   printf("&& O turn &&\n"); // 흰색알 player의 차례임을 알려주는 문장 출력 
+  	   printf("the number of O: \n"); //흰색알의 배치 상황  
+  	   printf("input coordinates: \n ");// 좌표를 입력하라는 문장 출력  
+  	   scanf("%i\n", &wR); //좌표 입력_  흰색알의 ROW  
+	   scanf("%i\n", &wC);  // 좌표 입력_  흰색알의 Column 
   	   
-		 do{
-		 	
-		 }
-  	   
-	   
-	    }
-  	//white player 함수 선언 
+	
+  	   i= wR; j=wC;
+  	   gameboard[i][j]= 'O';
+	   print_othello(gameboard); 
+	 
+  	// 검은 색 알의 차례일 경우 //
+	   printf("&& @ turn &&\n"); // 검정알 player의 차례임을 알려주는 문장 출력 
+  	   printf("the number of @: \n"); //검정알의 배치 상황  
+  	   printf("input coordinates: \n ");// 좌표를 입력하라는 문장 출력  
+  	   scanf("%i\n", &bR); //좌표 입력_  검정알의 ROW  
+	   scanf("%i\n", &bC);  // 좌표 입력_  검정알의 Column 
   	
+  	   i= bR; j= bC;
+  	   gameboard[i][j]= '@';
+	   print_othello(gameboard);
 	   
   
   	 
